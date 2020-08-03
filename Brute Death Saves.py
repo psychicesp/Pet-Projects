de67de602d607f9e03a39e8492cb601e476c0a15
Eric S Penrose <ericspenrose@gmailcom>
2020-08-03 09:24:10 -0500

    Update allowing for user input ratehr than rewriting program

--- Brute Death Saves.py
+++ Brute Death Saves.py
@@ -8,12 +8,15 @@ def d20():
     roll = rand(1,20)
     return roll
 def deathSaveComparisson (n):
+# These are counters, the lowercase represents passing the series of death saves and the capital represents bouncing back with 1HP
     monk = 0
     Monk = 0
     brute = 0
     Brute = 0
     regular = 0
     Regular = 0
+# The following are the Monks death saving throws.  It bounces back when rolling a 20 and adds proficiency to the total 
+# but the proficiency does not count towards a "Nat 20"
     for i in range(n):
         saves = 0
         fails = 0
@@ -38,6 +41,7 @@ def deathSaveComparisson (n):
             elif fails >= 3:
                 in_progress = False
                 break
+#The following are the Brutes death saving throws.  THey roll a d6 to go with the d20, if they and up to 20 it counts as a Nat 20
     for i in range(n):
         saves = 0
         fails = 0
@@ -63,6 +67,7 @@ def deathSaveComparisson (n):
             elif fails >= 3:
                 in_progress = False
                 break
+#The following are the regular series of death saving throws
     for i in range(n):
         saves = 0
         fails = 0
@@ -87,12 +92,15 @@ def deathSaveComparisson (n):
             elif fails >= 3:
                 in_progress = False
                 break
+#The following will print the results as a percentage of times rolled
     print("Each unit was brought to 0 HP #" + str(n) + " times")
     print("The Monk survived " + str(100*(monk/n)) + "% of the time and bounced right back " + str(100*(Monk/n)) + "% of the time.")
     print("The Brute survived " + str(100*(brute/n)) + "% of the time and bounced right back " + str(100*(Brute/n)) + "% of the time.")
-    print("The regular survival rate was " + str(100*(regular/n)) + "% of the time and that individual would bounce back " + str(100*(Regular/n)) + "% of the time.")
+    print("The regular feller survived " + str(100*(regular/n)) + "% of the time and would bounce back " + str(100*(Regular/n)) + "% of the time.")
 
-deathSaveComparisson(10000000)
+no_times = int(input("How many iterations should we simulate?"))
+
+deathSaveComparisson(no_times)
         
 
             
