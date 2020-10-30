4a2a7c7eb69a9f77e1e785f270890dba8f4e78ca
Eric S Penrose <ericspenrose@gmail.com>
2020-10-30 10:46:40 -0500

    Delete Test.py

--- Test.py
+++
@@ -1,5 +0,0 @@
-# This program is an initial test to wet my feet with Git repositories and Python Programming.
-
-response = input("What is your input?")
-
-print("What a stupid input! What kind of a person would type in \"" + response + "\"?!?!?!?" )
