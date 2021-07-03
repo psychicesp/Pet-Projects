7a103c8b54202a1b669b9ab41e2146706364220a
Eric S Penrose <ericspenrose@gmail.com>
2021-07-02 21:43:58 -0500

    did

--- dnd_client/dnd_client.py
+++ dnd_client/dnd_client.py
@@ -3,13 +3,12 @@ from splinter import Browser
 from bs4 import BeautifulSoup as BS
 import requests as req
 import pandas as pd
+import time
 from pprint import pprint
 
 # url = 'https://www.aidedd.org/dnd-filters/monsters.php'
 # executable_path = {'executable_path': 'chromedriver.exe'}
 # browser = Browser('chrome', **executable_path, headless=False)
-# browser.visit(url)
-# dnd_soup = BS(browser.html, 'html.parser')
 # browser.quit()
 # dnd_soup = dnd_soup.find_all("tbody")[0]
 # name_links = {}
@@ -17,16 +16,36 @@ from pprint import pprint
 #     name_links[bit.text] = bit['href']
 
 # pprint(name_links)
-# %%
-def url_finder(x):
-    x=x.lower().split(' ')
-    if len(x)>1:
-        x = '-'.join(x)
-    else:
-        x=x[0]
-    return f"https://www.aidedd.org/dnd/monstres.php?vo={x}"
+# def url_finder(x):
+#     x=x.lower().split(' ')
+#     if len(x)>1:
+#         x = '-'.join(x)
+#     else:
+#         x=x[0]
+#     return f"https://www.aidedd.org/dnd/monstres.php?vo={x}"
 df = pd.read_csv('monsters.csv')
 
-df['URL']=df['Name'].apply(url_finder)
+# df['URL']=df['Name'].apply(url_finder)
+
+def html_writer(x):
+    nameo=x.lower()
+    nameo = nameo.split(' ')
+    if len(nameo)>1:
+        nameo = '-'.join(nameo)
+    else:
+        nameo=nameo[0]
+    url = f"https://www.aidedd.org/dnd/monstres.php?vo={nameo}"
+    nameo=x.replace('/', ' OR ')
+    file_name = f"html_pages/{nameo}.html"
+    response = req.get(url)
+    time.sleep(0.5)
+    with open(file_name, 'w') as stuff:
+        stuff.write(response.text)
+
+
+names = df['Name'].to_list()
+
+for name in names:
+    html_writer(name)
 
 # %%
