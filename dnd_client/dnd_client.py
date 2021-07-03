01f14d03d6d6289c473d89e7fb9c4871b029bce9
Eric S Penrose <ericspenrose@gmail.com>
2021-07-03 11:29:31 -0500

    did

--- dnd_client/dnd_client.py
+++ dnd_client/dnd_client.py
@@ -6,39 +6,29 @@ import pandas as pd
 import time
 from pprint import pprint
 import random
-
-# url = 'https://www.aidedd.org/dnd-filters/monsters.php'
-# executable_path = {'executable_path': 'chromedriver.exe'}
-# browser = Browser('chrome', **executable_path, headless=False)
+#%%
+url = 'https://www.aidedd.org/dnd-filters/spells.php'
+executable_path = {'executable_path': 'chromedriver.exe'}
+browser = Browser('chrome', **executable_path, headless=False)
+browser.visit(url)
+# dnd_soup = BS(browser.html, 'html.parser')
 # browser.quit()
 # dnd_soup = dnd_soup.find_all("tbody")[0]
 # name_links = {}
 # for bit in dnd_soup.find_all("a",href=True):
 #     name_links[bit.text] = bit['href']
 
-# pprint(name_links)
-# def url_finder(x):
-#     x=x.lower().split(' ')
-#     if len(x)>1:
-#         x = '-'.join(x)
-#     else:
-#         x=x[0]
-#     return f"https://www.aidedd.org/dnd/monstres.php?vo={x}"
-df = pd.read_csv('monsters.csv')
-
+df = pd.read_html(browser.html)
+browser.quit()
 # df['URL']=df['Name'].apply(url_finder)
-
+# %%
+df = pd.read_csv('spells.csv')
 def html_writer(x):
-    nameo=x.lower()
-    nameo = nameo.split(' ')
-    if len(nameo)>1:
-        nameo = '-'.join(nameo)
-    else:
-        nameo=nameo[0]
-    url = f"https://www.aidedd.org/dnd/monstres.php?vo={nameo}"
+    nameo=x.lower().replace("'",'-').replace(' ','-')
+    url = f"https://www.aidedd.org/dnd/sorts.php?vo={nameo}"
     nameo=x.replace('/', ' OR ')
     print(f"Getting {x}")
-    file_name = f"html_pages/{nameo}.html"
+    file_name = f"html_pages/spells/{nameo}.html"
     response = req.get(url)
     rest = random.random() 
     rest = rest * random.randint(2,3)
@@ -47,10 +37,8 @@ def html_writer(x):
     time.sleep(rest)
     with open(file_name, 'w', encoding='utf-8') as stuff:
         stuff.write(response.text)
-    print(f"Failed to write {x}")
 
 names = df['Name'].to_list()
-
 for name in names:
     html_writer(name)
 
