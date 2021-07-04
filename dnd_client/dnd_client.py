268dfc14b5a68c9eb32c09c132690ca4e9123683
Eric S Penrose <ericspenrose@gmail.com>
2021-07-03 22:33:08 -0500

    did

--- dnd_client/dnd_client.py
+++ dnd_client/dnd_client.py
@@ -10,8 +10,12 @@ import shutil
 import os
 folder = 'html_pages/monsters'
 files = os.listdir(folder)
-#%%
-url = 'https://www.aidedd.org/dnd-filters/spells.php'
+
+
+# url = 'https://www.aidedd.org/dnd-filters/spells.php'
+
+
+
 # executable_path = {'executable_path': 'chromedriver.exe'}
 # browser = Browser('chrome', **executable_path, headless=False)
 # browser.visit(url)
@@ -42,10 +46,50 @@ url = 'https://www.aidedd.org/dnd-filters/spells.php'
 # for name in names:
 #     html_writer(name)
 
-# %%
+
+# for file in files:
+#     print(f'Working on {file}')
+#     location = f"{folder}/{file}"
+#     file_name = f"html_pages/French/monsters/{file}"
+#     try:
+#         with open(location, 'r') as thingy:
+#             stuff = thingy.read()
+#     except:
+#         with open(location, 'r', encoding='utf-8') as thingy:
+#             stuff = thingy.read()
+#     soup = BS(stuff,'html.parser')
+#     try:
+#         url = soup.find_all('link', {'rel':'alternate'})[0]['href']
+#         response = req.get(url)
+#         print(f"    Writing French {file}")
+#         try:
+#             with open(file_name, 'w', encoding='utf-8') as stuff:
+#                 stuff.write(response.text)
+#         except:
+#             with open(file_name, 'w') as stuff:
+#                 stuff.write(response.text)
+#         print(f"         -----{file} Was A Success!!!-----")
+#     except:
+#         pass
+#     rest = random.random() 
+#     rest = rest * random.randint(1,2)
+#     rest += 1
+#     print(f"            Resting for {round(rest,2)} seconds")
+#     time.sleep(rest)
+ 
+
+    # %%
+attrs = {
+    'STR':{},
+    'DEX':{},
+    'CON':{},
+    'INT':{},
+    'WIS':{},
+    'CHA':{}
+}
 for file in files:
-    print(f'Working on {file}')
     location = f"{folder}/{file}"
+    creature = file.split('.')[0]
     file_name = f"html_pages/French/monsters/{file}"
     try:
         with open(location, 'r') as thingy:
@@ -54,24 +98,51 @@ for file in files:
         with open(location, 'r', encoding='utf-8') as thingy:
             stuff = thingy.read()
     soup = BS(stuff,'html.parser')
-    try:
-        url = soup.find_all('link', {'rel':'alternate'})[0]['href']
-        response = req.get(url)
-        print(f"    Writing French {file}")
-        try:
-            with open(file_name, 'w', encoding='utf-8') as stuff:
-                stuff.write(response.text)
-        except:
-            with open(file_name, 'w') as stuff:
-                stuff.write(response.text)
-        print(f"         -----{file} Was A Success!!!-----")
-    except:
-        pass
-    rest = random.random() 
-    rest = rest * random.randint(1,2)
-    rest += 1
-    print(f"            Resting for {round(rest,2)} seconds")
-    time.sleep(rest)
- 
+    scores = soup.find_all('div', {'class':'carac'})
+    for score in scores:
+        score = score.text.split(' ')[0]
+        att = score[:3]
+        sco = int(score[3:])
+        attrs[att][creature] = sco
 
-    # %%
+# %%
+df = pd.read_csv('monsters.csv')
+
+def strengthener(x):
+    if x.Name in attrs['STR'].keys():
+        return attrs['STR'][x.Name]
+    else:
+        return 0
+def dexerciser(x):
+    if x.Name in attrs['DEX'].keys():
+        return attrs['DEX'][x.Name]
+    else:
+        return 0
+def congratulater(x):
+    if x.Name in attrs['CON'].keys():
+        return attrs['CON'][x.Name]
+    else:
+        return 0
+def teacher(x):
+    if x.Name in attrs['INT'].keys():
+        return attrs['INT'][x.Name]
+    else:
+        return 0
+def guru(x):
+    if x.Name in attrs['WIS'].keys():
+        return attrs['WIS'][x.Name]
+    else:
+        return 0
+def coach(x):
+    if x.Name in attrs['CHA'].keys():
+        return attrs['CHA'][x.Name]
+    else:
+        return 0
+
+df['Str'] = df.apply(strengthener, axis = 1)
+df['Dex'] = df.apply(dexerciser, axis = 1)
+df['Con'] = df.apply(congratulater, axis = 1)
+df['Int'] = df.apply(teacher, axis = 1)
+df['Wis'] = df.apply(guru, axis = 1)
+df['Cha'] = df.apply(coach, axis = 1)
+# %%
