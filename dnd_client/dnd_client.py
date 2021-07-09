0ebd9095350d998db6aa1cb981b01f6c3320bb12
Eric S Penrose <ericspenrose@gmail.com>
2021-07-08 20:07:22 -0500

    did

--- dnd_client/dnd_client.py
+++ dnd_client/dnd_client.py
@@ -9,7 +9,7 @@ from pprint import pprint
 import random
 import shutil
 import os
-folder = 'html_pages/monsters'
+folder = 'html_pages/spells'
 files = os.listdir(folder)
 
 #%%
@@ -79,18 +79,122 @@ files = os.listdir(folder)
 #     time.sleep(rest)
  
 
-    # %%
+#     # %%
+# attrs = {
+#     'ac':{},
+#     'ac_source':{},
+#     'speed':{},
+#     'hit_dice_type':{},
+#     'hit_dice':{}
+
+# }
+# for file in files:
+#     location = f"{folder}/{file}"
+#     creature = file.split('.')[0]
+#     file_name = f"html_pages/monsters/{file}"
+#     try:
+#         with open(location, 'r') as thingy:
+#             stuff = thingy.read()
+#     except:
+#         with open(location, 'r', encoding='utf-8') as thingy:
+#             stuff = thingy.read()
+#     soup = BS(stuff,'html.parser')
+#     # print(creature)
+#     try:
+#         score = soup.find_all('div', {'class':'red'})[0]
+#         score = str(score).split('<svg><polyline points=')[0]
+#         score = score.replace('<br>','').replace('<br/>','').replace('<','').replace('>','').replace('/','')
+#         score = score.split('strong')
+#         # print(score[6])
+#         if '(' in score[2]:
+#             ac = score[2].split('(')[0].replace(' ','')
+#             ac_source = score[2].split('(')[1].replace(')','').strip(' ')
+#             if 'mage armor' in ac_source and ac_source.split(' ')[0].strip(' ').isnumeric():
+#                 ac = int(ac_source.split(' ')[0].strip(' '))
+#                 ac_source = "mage armor"
+#         elif 'embarkskinem' in ac_source.split(" ") and ac <= 16:
+#             ac = 16
+#             ac_source = "barkskin"
+#         else:
+#             ac = score[2].replace(' ','')
+#             try:
+#                 ac = int(ac)
+#             except:
+#                 ac = pd.NA
+#             ac_source = pd.NA
+#         hit_dice = score[4].split('(')[1].split('d')[0]
+#         hit_dice_type = score[4].split('(')[1].split('d')[1].split(' ')[0]
+#         hit_dice_type = 'd'+ hit_dice_type.strip('(').strip(')')
+#         speed = score[6].replace('div','').replace('.','')
+#     #     print(f"""    ac = {ac}
+#     # ac_source = {ac_source}
+#     # hit_dice = {hit_dice}
+#     # hit_dice_type = {hit_dice_type}
+#     # speed = {speed}""")
+#     except:
+#         score = 'error'
+#         ac = pd.NA
+#         ac_source = pd.NA
+#         hit_dice = pd.NA
+#         hit_dice_type = pd.NA
+#         speed = pd.NA
+#         print(f"Could not parse {creature}")
+#     attrs['ac'][creature] = ac
+#     attrs['ac_source'][creature] = ac_source
+#     attrs['speed'][creature] = speed
+#     attrs['hit_dice'][creature] = hit_dice
+#     attrs['hit_dice_type'][creature] = hit_dice_type
+# #%%
+# df = pd.read_csv('monsters.csv')
+
+# def armorer(x):
+#     if x.Name in attrs['ac'].keys():
+#         return attrs['ac'][x.Name]
+#     else:
+#         return pd.NA
+# def armoring(x):
+#     if x.Name in attrs['ac_source'].keys():
+#         return attrs['ac_source'][x.Name]
+#     else:
+#         return pd.NA
+# def speeder(x):
+#     if x.Name in attrs['speed'].keys():
+#         return attrs['speed'][x.Name]
+#     else:
+#         return pd.NA
+# def hitter(x):
+#     if x.Name in attrs['hit_dice'].keys():
+#         return attrs['hit_dice'][x.Name]
+#     else:
+#         return pd.NA
+# def hitting(x):
+#     if x.Name in attrs['hit_dice_type'].keys():
+#         return attrs['hit_dice_type'][x.Name]
+#     else:
+#         return pd.NA
+
+# df['AC'] = df.apply(armorer, axis = 1)
+# df['Armor'] = df.apply(armoring, axis = 1)
+# df['HitDice#'] = df.apply(hitter, axis = 1)
+# df['HitDice'] = df.apply(hitting, axis = 1)
+# df['Speed'] = df.apply(speeder, axis = 1)
+
+# def stripper(x):
+#     try:
+#         return x.strip(' ')
+#     except:
+#         return pd.NA
+
+# df['Speed'] = df['Speed'].apply(stripper)
+
+# %%
 attrs = {
-    'ac':{},
-    'ac_source':{},
-    'speed':{},
-    'hit_dice_type':{},
-    'hit_dice':{}
 
 }
-for file in files:
+
+for file in files[:1]:
     location = f"{folder}/{file}"
-    creature = file.split('.')[0]
+    spell = file.split('.')[0]
     file_name = f"html_pages/monsters/{file}"
     try:
         with open(location, 'r') as thingy:
@@ -102,88 +206,11 @@ for file in files:
     # print(creature)
     try:
         score = soup.find_all('div', {'class':'red'})[0]
-        score = str(score).split('<svg><polyline points=')[0]
+        score = str(score).split('<svg><polyline points=')[1]
         score = score.replace('<br>','').replace('<br/>','').replace('<','').replace('>','').replace('/','')
         score = score.split('strong')
-        # print(score[6])
-        if '(' in score[2]:
-            ac = score[2].split('(')[0].replace(' ','')
-            ac_source = score[2].split('(')[1].replace(')','').strip(' ')
-            if 'mage armor' in ac_source and ac_source.split(' ')[0].strip(' ').isnumeric():
-                ac = int(ac_source.split(' ')[0].strip(' '))
-                ac_source = "mage armor"
-            elif 'eembarkskinem' in ac_source and ac <= 16:
-                ac = 16
-                ac_source = "barkskin"
-        else:
-            ac = score[2].replace(' ','')
-            try:
-                ac = int(ac)
-            except:
-                ac = pd.NA
-            ac_source = pd.NA
-        hit_dice = score[4].split('(')[1].split('d')[0]
-        hit_dice_type = score[4].split('(')[1].split('d')[1].split(' ')[0]
-        hit_dice_type = 'd'+ hit_dice_type.strip('(').strip(')')
-        speed = score[6].replace('div','').replace('.','')
-    #     print(f"""    ac = {ac}
-    # ac_source = {ac_source}
-    # hit_dice = {hit_dice}
-    # hit_dice_type = {hit_dice_type}
-    # speed = {speed}""")
+        print(score)
     except:
-        score = 'error'
-        ac = pd.NA
-        ac_source = pd.NA
-        hit_dice = pd.NA
-        hit_dice_type = pd.NA
-        speed = pd.NA
-        print(f"Could not parse {creature}")
-    attrs['ac'][creature] = ac
-    attrs['ac_source'][creature] = ac_source
-    attrs['speed'][creature] = speed
-    attrs['hit_dice'][creature] = hit_dice
-    attrs['hit_dice_type'][creature] = hit_dice_type
+        print(f"Could not parse {spell}")
+
 #%%
-df = pd.read_csv('monsters.csv')
-
-def armorer(x):
-    if x.Name in attrs['ac'].keys():
-        return attrs['ac'][x.Name]
-    else:
-        return pd.NA
-def armoring(x):
-    if x.Name in attrs['ac_source'].keys():
-        return attrs['ac_source'][x.Name]
-    else:
-        return pd.NA
-def speeder(x):
-    if x.Name in attrs['speed'].keys():
-        return attrs['speed'][x.Name]
-    else:
-        return pd.NA
-def hitter(x):
-    if x.Name in attrs['hit_dice'].keys():
-        return attrs['hit_dice'][x.Name]
-    else:
-        return pd.NA
-def hitting(x):
-    if x.Name in attrs['hit_dice_type'].keys():
-        return attrs['hit_dice_type'][x.Name]
-    else:
-        return pd.NA
-
-df['AC'] = df.apply(armorer, axis = 1)
-df['Armor'] = df.apply(armoring, axis = 1)
-df['HitDice#'] = df.apply(hitter, axis = 1)
-df['HitDice'] = df.apply(hitting, axis = 1)
-df['Speed'] = df.apply(speeder, axis = 1)
-# %%
-
-def stripper(x):
-    try:
-        return x.strip(' ')
-    except:
-        return pd.NA
-
-# %%
