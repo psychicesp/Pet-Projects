646ba2f710dadae08e1a9cee7067da8797b4f712
Eric S Penrose <ericspenrose@gmail.com>
2021-08-13 16:49:15 -0500

    did

--- dnd_client_5e_Tools/dnd_client_new.py
+++ dnd_client_5e_Tools/dnd_client_new.py
@@ -9,38 +9,64 @@ from pprint import pprint
 import random
 import shutil
 import os
+import json
 
 # %%
-url = "https://5e.tools/items.html#abacus_phb"
+# url = "https://5e.tools/items.html#abacus_phb"
 
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
-browser.visit(url)
-time.sleep(5)
-dnd_soup = BS(browser.html, 'html.parser')
+# executable_path = {'executable_path': 'chromedriver.exe'}
+# browser = Browser('chrome', **executable_path, headless=False)
+# browser.visit(url)
+# time.sleep(5)
+# dnd_soup = BS(browser.html, 'html.parser')
 
-dnd_table = dnd_soup.find_all("a", {"class":"lst--border lst__row-inner"})
-hrefs = [x['href'] for x in dnd_table]
-print(type(hrefs[0]))
+# dnd_table = dnd_soup.find_all("a", {"class":"lst--border lst__row-inner"})
+# hrefs = [x['href'] for x in dnd_table]
+# print(type(hrefs[0]))
+# #%%
+# url = "https://5e.tools/items.html"
+# c = 0
+# for href in hrefs[365:]:
+#     c += 1
+#     name = href.split("#")[1].split("_")[0].replace('%20','_').replace('*','x')
+#     new_url = url+href
+#     print(name)
+#     browser.visit(new_url)
+#     if c < 2:    
+#         time.sleep(5)
+#     else:
+#         time.sleep(2)
+#     dnd_soup = BS(browser.html, 'html.parser')
+#     dnd_soup = dnd_soup.find("table", {"class": "stats"})
+#     with open(f"html_pages/items/{name}.html", "w") as temp:
+#         pprint(dnd_soup.encode('utf8'), stream = temp)
+#     print("     ", "finished")
+# browser.quit()
 #%%
-url = "https://5e.tools/items.html"
-c = 0
-for href in hrefs[365:]:
-    c += 1
-    name = href.split("#")[1].split("_")[0].replace('%20','_').replace('*','x')
-    new_url = url+href
-    print(name)
-    browser.visit(new_url)
-    if c < 2:    
-        time.sleep(5)
-    else:
-        time.sleep(2)
-    dnd_soup = BS(browser.html, 'html.parser')
-    dnd_soup = dnd_soup.find("table", {"class": "stats"})
-    with open(f"html_pages/items/{name}.html", "w") as temp:
-        pprint(dnd_soup.encode('utf8'), stream = temp)
-    print("     ", "finished")
-browser.quit()
+def monster_unpacker(file):
+    monster_json={}
 #%%
 
-#%%
+with open('html_pages/monsters/aarakocra.html', 'r') as mon:
+    mon_soup = BS(mon, 'html.parser')
+
+ability_soup = mon_soup.find_all('span', {'class':'roller render-roller'})
+pprint(ability_soup)
+# %%
+def html_cleaner(in_folder):
+    files = os.listdir(in_folder)
+    for file in files:
+        with open(f"{in_folder}/{file}", 'r') as dirty:
+            clean = str(dirty.read()).replace("' b'", '').replace("\" b'", '').replace("' b\"", '').replace("'\nb'", '').replace("""'
+b'""", '').replace("""'
+ b'""", '').replace("""\"
+b'""", '').replace("""'
+ b\"""", '').replace("""'
+ b'""", '').replace("""'
+b\"""", '').replace('''"
+b"''', '').replace("(b'", "")
+        with open(f"{in_folder}/{file}", 'w') as new:
+            new.write(clean)
+
+html_cleaner('html_pages/monsters')
+# %%
