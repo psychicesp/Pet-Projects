6254367bf0af97e322ad2300d97d1214255733b5
Eric S Penrose <ericspenrose@gmail.com>
2021-08-13 14:41:48 -0500

    did

--- dnd_client_5e_Tools/dnd_client_new.py
+++ dnd_client_5e_Tools/dnd_client_new.py
@@ -24,27 +24,28 @@ hrefs = [x['href'] for x in dnd_table]
 print(type(hrefs[0]))
 #%%
 url = "https://5e.tools/bestiary.html"
-for href in hrefs:
+c = 0
+for href in hrefs[160:]:
+    c += 1
     name = href.split("#")[1].split("_")[0].replace('%20','_')
     new_url = url+href
     print(name)
-    browser.visit(new_url)    
-    time.sleep(5)
-    try:
-        browser.find_by_value('Statblock').click()
-    except:
-        pass
+    browser.visit(new_url)
+    if c < 2:    
+        time.sleep(5)
+    else:
+        time.sleep(2)
     dnd_soup = BS(browser.html, 'html.parser')
     dnd_soup = dnd_soup.find("table", {"class": "stats monster"})
     with open(f"html_pages/monsters/{name}.html", "w") as temp:
-        pprint(dnd_soup, stream = temp)
+        pprint(dnd_soup.encode('utf8'), stream = temp)
     try:
+        browser.find_by_value('Info').click()
         time.sleep(2)
         info_soup = BS(browser.html, 'html.parser')
         info_soup = info_soup.find("td", {"colspan":"6", "class":"text"})
-        pprint(info_soup)
         with open(f"html_pages/info/{name}.html", "w") as temp:
-            pprint(info_soup, stream = temp)
+            pprint(info_soup.encode('utf8'), stream = temp)
     except:
         pass
     try:
@@ -61,28 +62,12 @@ for href in hrefs:
             f.write(req.get(image_url).content)
     except:
         pass
+    try:
+        browser.find_by_value('Statblock').click()
+    except:
+        pass
     print("     ", "finished")
 browser.quit()
 #%%
-name_links = {}
-for bit in dnd_soup.find_all("a",href=True):
-    name_links[bit.text] = bit['href']
-
-df = pd.read_html(browser.html)
-browser.quit()
-df['URL']=df['Name'].apply(url_finder)
-
-df = pd.read_csv('spells.csv')
-def html_writer(x, url):
-    nameo=x.lower().replace("'",'-').replace(' ','-')
-    url = f"https://www.aidedd.org/dnd/sorts.php?vo={nameo}"
-    nameo=x.replace('/', ' OR ')
-    print(f"Getting {x}")
-    file_name = f"html_pages/spells/{nameo}.html"
-    response = req.get(url)
-
-    with open(file_name, 'w', encoding='utf-8') as stuff:
-        stuff.write(response.text)
-
 
 #%%
